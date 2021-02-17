102691d4e45ccacb220b2c85e8f803ce9efcb325
unknown <sand8594@hufs.ac.kr>
2021-02-17 13:42:09 +0900

    BOJ 11413

--- BOJ 15781/BOJ 15781.py
+++ BOJ 15781/BOJ 15781.py
@@ -4,12 +4,12 @@ helmet = list(map(int, input().split()))
 vest = list(map(int, input().split()))
 
 scores = list()
-for i in range(int(n)):
+for i in range(int(n)):  # 가능한 조합 다 만들기
     for j in range(int(m)):
         scores.append(helmet[i] + vest[j])
 
 max = 0
-for i in range(len(scores)):
+for i in range(len(scores)):  # 가장 큰 방어력 구하기
     if(scores[i] > max):
         max = scores[i]
 
