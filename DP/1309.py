7aa5537ff1f30451ed55960c215cd55c146ff03f
juhui88 <sand8594@naver.com>
2024-08-20 16:36:08 +0900

    BOJ 1309

--- DP/1309.py
+++ DP/1309.py
@@ -3,6 +3,15 @@ input = sys.stdin.readline
 
 N = int(input())
 
-lion = 0
+dp = [[1,1,1] for _ in range(N+1)]
 
-while():
+for i in range(2,N+1):
+    for j in range(3):
+        if j == 0:
+            dp[i][j] = sum(dp[i-1]) % 9901
+        elif j==1 :
+            dp[i][j] =(dp[i-1][j-1] + dp[i-1][j+1] ) % 9901
+        else:
+            dp[i][j] = (dp[i-1][j-2] + dp[i-1][j-1]) % 9901
+
+print(sum(dp[-1]) % 9901)
