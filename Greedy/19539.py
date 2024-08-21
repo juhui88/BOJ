b947a9e4fc5763167a3a1d65d60556105996318b
juhui88 <sand8594@naver.com>
2024-08-21 16:39:31 +0900

    BOJ 19539

--- Greedy/19539.py
+++ Greedy/19539.py
@@ -4,17 +4,15 @@ input = sys.stdin.readline
 N = int(input())
 trees = list(map(int,input().split()))
 
-if sum(trees)%3 == 0:
-    one_n = 0
-    two_n = 0
-    for item in trees:
-        two_n += item // 2
-        one_n += item % 2
-    if one_n == two_n: print("Yes")
-    elif one_n < two_n:
-        two_n -= one_n
-        if two_n % 3 == 0: print("YES")
-        else: print("NO")
-    else: print("NO") 
-else:
+sum_trees = sum(trees)
+if sum_trees % 3 != 0:
     print("NO")
+else:
+    cnt = 0
+    for i in trees:
+        cnt += i // 2
+
+    if cnt >= (sum_trees // 3):
+        print("YES")
+    else:
+        print("NO")
